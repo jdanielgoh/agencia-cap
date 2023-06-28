32e1a213800888580f15885ade2a484d2490f8c9
ciretorres <erictorres.velasco@gmail.com>
2023-06-28 14:49:15 -0600

    script de Andrés que cambia los títulos de la página

--- src/router/index.js
+++ src/router/index.js
@@ -41,4 +41,9 @@ const router = new VueRouter({
   },
 })
 
+router.beforeEach((to, from, next) => {
+  document.title = `${process.env.VUE_APP_TITLE} | ${to.name}`
+  next()
+})
+
 export default router
