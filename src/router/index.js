821b1e2da0b2cc742b2b222bae2ff0428d1fcb5a
ciretorres <erictorres.velasco@gmail.com>
2023-06-29 10:40:11 -0600

    se agrega script para mostrar los títulos dependiendo las vistas

--- src/router/index.js
+++ src/router/index.js
@@ -41,6 +41,7 @@ const router = new VueRouter({
   },
 })
 
+// Muestra el título de la página dependiendo de las vistas
 router.beforeEach((to, from, next) => {
   document.title = `${process.env.VUE_APP_TITLE} | ${to.name}`
   next()
