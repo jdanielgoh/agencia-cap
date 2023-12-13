4720849432559dbefa941966cdc876f9a6ea717d
paulina.pradel <crip.dadsig019@conacyt.mx>
2023-12-13 04:34:04 +0000

    Update 7 files
    - /src/main.js
    - /src/store/index.js
    - /src/router/index.js
    - /src/App.vue
    - /src/views/PaginaInicio.vue
    - /src/views/ErrorPaginaNoEncontrada.vue
    - /src/components/navegacion/NavegacionPrincipalBase.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
+<!--This file is part of sisdai-proyecto-base.-->
+
+<!--sisdai-proyecto-base is free software: you can redistribute it and/or modify-->
+<!--it under the terms of the GNU Lesser General Public License as published by the-->
+<!--Free Software Foundation, either version 3 of the License, or-->
+<!--(at your option) any later version.-->
+
+<!--sisdai-proyecto-base is distributed in the hope that it will be useful,-->
+<!--but WITHOUT ANY WARRANTY; without even the implied warranty of-->
+<!--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General-->
+<!--Public License for more details.-->
+
+<!--You should have received a copy of the GNU Lesser General Public License along-->
+<!--with sisdai-proyecto-base. If not, see <https://www.gnu.org/licenses/>.-->
+
+
 <script setup>
 import store from '@/store'
 import { onMounted, onUpdated, watch, ref, nextTick } from 'vue'
