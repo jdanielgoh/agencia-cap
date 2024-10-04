4d02c975e9ecb0f3455a6b0e92c41195d6bd604c
flkt <flkt.crnpio@gmail.com>
2024-10-03 19:44:22 -0600

    cambiando a vue 3

---
+++ src/AppBACKUP.vue
@@ -0,0 +1,98 @@
+<!--This file is part of sisdai-proyecto-base.-->
+
+<!--sisdai-proyecto-base is free software: you can redistribute it and/or modify-->
+<!--it under the terms of the GNU Lesser General Public License as published by the-->
+<!--Free Software Foundation, either version 3 of the License, or-->
+<!--(at your option) any later version.-->
+
+<!--sisdai-proyecto-base is distributed in the hope that it will be useful,-->
+<!--but WITHOUT ANY WARRANTY; without even the implied warranty of-->
+<!--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General-->
+<!--Public License for more details.-->
+
+<!--You should have received a copy of the GNU Lesser General Public License along-->
+<!--with sisdai-proyecto-base. If not, see <https://www.gnu.org/licenses/>.-->
+
+<script setup>
+import store from '@/store'
+import { onMounted, watch, ref, nextTick } from 'vue'
+import { useRoute, useRouter } from 'vue-router/composables'
+const ruta = useRoute()
+const router = useRouter()
+import NavegacionPrincipalBase from './components/navegacion/NavegacionPrincipalBase.vue'
+
+const infoDespliegue = {
+  version_proyecto: process.env.PACKAGE_VERSION,
+  actualizacion_proyecto: process.env.DATE_DEPLOY,
+  entorno_proyecto: process.env.ENV_DEPLOY,
+}
+const entradaInicial = ref(true)
+
+// Agrega aria-hidden al elemento con icono
+onMounted(async () => {
+  await router.onReady(() => {
+    ruta.path === '/' ? (entradaInicial.value = false) : null
+  })
+})
+
+// Con este watch definimos a donde se moverá el focus al cambiar de ruta
+watch(
+  () => ruta.path,
+  () => {
+    if (!entradaInicial.value) {
+      if (ruta.path) {
+        nextTick(() => {
+          moviendoFocoNavegacion(ruta.path)
+        })
+      }
+    } else {
+      entradaInicial.value = false
+    }
+  }
+)
+
+function moviendoFocoNavegacion(path) {
+  path
+  //let carpetas = path.split('/').filter(d => d != '')
+  const elemento_receptor = document.querySelector('#principal')
+  if (elemento_receptor) {
+    elemento_receptor.tabIndex = '-1'
+    elemento_receptor.focus()
+  }
+}
+</script>
+
+<template>
+  <div
+    id="app"
+    :class="store.getters['accesibilidad/clasesAccesibles']"
+  >
+    <a
+      href="#principal"
+      class="ir-contenido-principal"
+      >Ir a contenido principal</a
+    >
+    <SisdaiNavegacionGobMx />
+    <NavegacionPrincipalBase />
+
+    <router-view />
+
+    <SisdaiMenuAccesibilidad :objetoStore="store" />
+    <SisdaiInfoDeDespliegue
+      :versionProyecto="infoDespliegue.version_proyecto"
+      :entornoProyecto="infoDespliegue.entorno_proyecto"
+      :actualizacionProyecto="infoDespliegue.actualizacion_proyecto"
+    />
+    <SisdaiPiePaginaConahcyt />
+    <SisdaiPiePaginaGobMx />
+  </div>
+</template>
+
+<style lang="scss">
+// Estilos de la biblioteca
+@import 'node_modules/sisdai-css/src/sisdai.scss';
+
+.overflow-hidden {
+  overflow: hidden;
+}
+</style>
--- src/components/navegacion/NavegacionPrincipalBase.vue
+++ src/components/navegacion/NavegacionPrincipalBase.vue
@@ -1,41 +1,31 @@
-<!--This file is part of sisdai-proyecto-base.-->
+<script setup>
+import { RouterLink, useRoute } from 'vue-router'
+import { computed } from 'vue'
 
-<!--sisdai-proyecto-base is free software: you can redistribute it and/or modify-->
-<!--it under the terms of the GNU Lesser General Public License as published by the-->
-<!--Free Software Foundation, either version 3 of the License, or-->
-<!--(at your option) any later version.-->
-
-<!--sisdai-proyecto-base is distributed in the hope that it will be useful,-->
-<!--but WITHOUT ANY WARRANTY; without even the implied warranty of-->
-<!--MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General-->
-<!--Public License for more details.-->
-
-<!--You should have received a copy of the GNU Lesser General Public License along-->
-<!--with sisdai-proyecto-base. If not, see <https://www.gnu.org/licenses/>.-->
-
-<script setup></script>
+const route = useRoute()
+const nombre = computed(() => route.name)
+</script>
 
 <template>
-  <SisdaiNavegacionPrincipal
-    :nav-informacion="`Sección: <b>${$route.name}</b>`"
-  >
-    <template #complementario>
-      <a
-        href="#"
-        class="nav-hipervinculo"
-        target="_blank"
-      >
-        <b>IR A ENLACE EXTERNO</b>
-      </a>
-    </template>
+  <SisdaiNavegacionPrincipal :nav-informacion="nombre">
     <ul class="nav-menu">
       <li>
-        <a
-          href="#"
+        <RouterLink
           class="nav-hipervinculo"
+          exact
+          to="/"
         >
           Inicio
-        </a>
+        </RouterLink>
+      </li>
+      <li>
+        <RouterLink
+          class="nav-hipervinculo"
+          exact
+          to="/otra-vista"
+        >
+          Otra Vista
+        </RouterLink>
       </li>
     </ul>
   </SisdaiNavegacionPrincipal>
